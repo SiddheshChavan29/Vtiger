cde5f78aec336e17478e2cef79ebed726cf3ed5d
SiddheshChavan29 <126074451+SiddheshChavan29@users.noreply.github.com>
2023-03-30 11:13:29 +0530

    Add files via upload

---
+++ Byteconversion.java
@@ -0,0 +1,20 @@
+class Shortconversion
+{
+ public static void main(String[]args) 
+  {
+	short s = 50 ;
+	int i = s ;
+	long l = s ;
+	float f = s ;
+	double d = s ;
+	char c = s ;
+    
+
+    System.out.println(i) ;
+	System.out.println(l) ;
+	System.out.println(f) ;
+	System.out.println(d) ;
+	System.out.println(c) ;
+  }
+}
+  
